18538575567895d1060ebd2c00dfbf2e240818b5
AdamB777 <akrupa777@gmail.com>
2023-12-03 19:21:00 +0100

    add register and login and tokenservice

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using Application.DTOs.LoginsDTO;
+using Application.DTOs.RegistersDTO;
+using Application.DTOs.UsersDTO;
+using Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ModelsDB;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly TokenService _tokenService;
+
+        public AccountController(UserManager<User> userManager, TokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user,loginDTO.Password)) 
+                return Unauthorized();
+
+            return new UserDTO
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user)
+            };
+        }
+        [HttpPost("registerpatient")]
+        public async Task<ActionResult> RegisterPatient(RegisterDTO registerDTO)
+        {
+            var user = new User {  Email = registerDTO.Email };
+
+            var result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Patient");
+
+            return StatusCode(201);
+        } [HttpPost("registerdietician")]
+        public async Task<ActionResult> RegisterDietician(RegisterDTO registerDTO)
+        {
+            var user = new User { Email = registerDTO.Email };
+
+            var result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Dietetician");
+
+            return StatusCode(201);
+        }
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserDTO>> GetCurrentUser()
+        {
+            var user=await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return new UserDTO
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user)
+            };
+        }
+    }
+}
---
+++ Application/DTOs/LoginsDTO/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.LoginsDTO
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
